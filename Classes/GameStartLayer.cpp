56c4777652d6568cff897c8fcbb8e0d7cb1f3061
Killer <87719349@qq.com>
2014-12-02 12:22:41 +0800

    add file

--- Classes/GameStartLayer.cpp
+++ Classes/GameStartLayer.cpp
@@ -1,5 +1,6 @@
 #include "GameStartLayer.h"
 #include "GameLayer.h"
+#include "VisibleRect.h"
 
 bool GameStartLayer::init()
 {
@@ -7,7 +8,7 @@ bool GameStartLayer::init()
 	{
 		return false;
 	}
-	UILayer* UIGameStart = UILayer::create();
+	/*UILayer* UIGameStart = UILayer::create();
 	this->addChild(UIGameStart);
 	m_GameStart = (Layout *)GUIReader::shareReader()->widgetFromJsonFile("GameStart/GameStart_1.ExportJson");
 	UIGameStart->addWidget(m_GameStart);
@@ -17,10 +18,54 @@ bool GameStartLayer::init()
 	UIExit = (UIButton*)m_GameStart->getChildByName("Button_exit");
 	UIExit->setTag(2);
 	UIStart->addTouchEventListener(UIStart,SEL_TouchEvent(&GameStartLayer::ButtonCallback));
-	UIExit->addTouchEventListener(UIStart,SEL_TouchEvent(&GameStartLayer::ButtonCallback));
+	UIExit->addTouchEventListener(UIStart,SEL_TouchEvent(&GameStartLayer::ButtonCallback));*/
+	CCArmatureDataManager::sharedArmatureDataManager()->addArmatureFileInfo("Main_Interface/Main_Interface0.png","Main_Interface/Main_Interface0.plist","Main_Interface/Main_Interface.ExportJson");
+	CCArmature* pMainInterface = CCArmature::create("Main_Interface");
+	CCArmature* pEyes = CCArmature::create("Main_Interface");
+	pMainInterface->getAnimation()->playByIndex(0);
+	pEyes->getAnimation()->playByIndex(1);
+	pMainInterface->setPosition(VisibleRect::center());
+	pEyes->setPosition(VisibleRect::center());
+	CCMenuItemImage* pMenuNewGame = CCMenuItemImage::create("Main_Interface/newgame.png","Main_Interface/newgame2.png",this,menu_selector(GameStartLayer::newGame));
+	CCMenuItemImage* pMenuStart = CCMenuItemImage::create("Main_Interface/start.png","Main_Interface/start2.png",this,menu_selector(GameStartLayer::Start));
+	pEyes->setZOrder(10000);
+	CCMenu* menu = CCMenu::create();
+	menu->addChild(pMenuNewGame);
+	menu->addChild(pMenuStart);
+	this->addChild(menu);
+	menu->setPosition(VisibleRect::center());
+	//menu->setZOrder(10000);
+	CCBone* pNewGame = pMainInterface->getBone("Layer12");
+	pNewGame->addDisplay(pMenuNewGame,1);
+	pNewGame->changeDisplayByIndex(1,true);
+	CCBone* pStart = pMainInterface->getBone("Layer13");
+	pStart->addDisplay(pMenuStart,1);
+	pStart->changeDisplayByIndex(1,true);
+	
+	this->addChild(pEyes);
+	
+	this->addChild(pMainInterface);
+
+
 	return true;
 }
 
+void GameStartLayer::newGame(CCObject *sender)
+{
+	//To Do Something
+	CCScene* pScene = CCScene::create();
+	pScene->addChild(GameLayer::create());
+	CCDirector::sharedDirector()->replaceScene(CCTransitionTurnOffTiles::create(0.5,pScene));
+	CCLog("new game"); 
+}
+
+void GameStartLayer::Start(CCObject *sender)
+{
+	//To Do Something
+	CCLog("start"); 
+}
+
+/*
 void GameStartLayer::ButtonCallback(CCObject* sender, TouchEventType type)
 {
 	if (type!=TouchEventType(TOUCH_EVENT_ENDED))
@@ -43,4 +88,4 @@ void GameStartLayer::ButtonCallback(CCObject* sender, TouchEventType type)
 #endif
 #endif
 	}
-}
+}*/
--- Classes/GameStartLayer.h
+++ Classes/GameStartLayer.h
@@ -6,13 +6,23 @@ USING_NS_CC;
 USING_NS_CC_EXT;
 class GameStartLayer :public CCLayer
 {
+	//构造函数
 public:
-	Layout *m_GameStart;
+	/*Layout *m_GameStart;
 	bool init();
 	UIButton* UIStart;
 	UIButton* UIExit;
-	void ButtonCallback(CCObject* sender, TouchEventType type);
+	void ButtonCallback(CCObject* sender, TouchEventType type);*/
+	//方法
+public:
 	CREATE_FUNC(GameStartLayer);
+	bool init();
+	void newGame(CCObject *sender);
+	void Start(CCObject *sender);
+
+	//字段
+public:
+
 private:
 };
 #endif
